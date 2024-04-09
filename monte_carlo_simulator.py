c189cbfb2b2a81d3c925b6911fecab5b905908a1
Rayhanul Islam <r97yhan@gmail.com>
2024-04-09 04:58:40 -0500

    Monte carlo simulation

---
+++ monte_carlo_simulator.py
@@ -0,0 +1,45 @@
+
+from moment_r2dp import * 
+
+
+
+class MonteCarlo:
+
+    def __init__(self, num_trails):
+        self.trails=num_trails
+
+    def simulate(self, gamma_params):
+
+        number_of_parameters = 20
+        dynamic_r2dp = DynamicMomentR2DP(number_of_parameters)
+        
+
+        results=[]
+        for _ in range(self.trails):
+            sigma = np.random.gamma(*gamma_params['sigma'])
+            delta = np.random.gamma(*gamma_params['delta'])
+            total_epsilon = np.random.gamma(*gamma_params['total_epsilon'])
+
+            result = dynamic_r2dp.get_R2DP_nosies(sigma, delta, total_epsilon)
+
+            results.append(result)
+
+        return results
+
+
+
+
+
+if __name__=="__main__":
+
+    num_trails=100
+    gamma_params = {
+    'sigma': (2, 0.5),  
+    'delta': (2, 0.1),  
+    'total_epsilon': (2, 1.0)  
+}
+    
+    monte_carlo = MonteCarlo(num_trails)
+
+    simulation_results = monte_carlo.simulate(gamma_params)
+
