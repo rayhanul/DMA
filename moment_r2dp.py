361b69634226d7eb8c9a9317a5cc66aebaefa550
Rayhanul Islam <r97yhan@gmail.com>
2024-04-11 20:10:38 -0500

    Update moment_r2dp.py

--- moment_r2dp.py
+++ moment_r2dp.py
@@ -51,12 +51,12 @@ class DynamicMomentR2DP:
 
         return max(value_1, value_2)    
         
-    def M(self, k, theta, x):
+    def M(self, k, theta, alpha):
         """
         return the moment generating function of a gamma distribution for t : (1-(x * theta))^(-k)
         """
 
-        return np.power((1- np.multiply(x, theta)), -k)
+        return np.power((1- np.multiply(alpha, theta)), -k)
 
     def get_l1_R2DP(self, k, theta, previous_utility):
         """
@@ -73,16 +73,24 @@ class DynamicMomentR2DP:
             return all_mgf_gammas
         
         integral_value, error_estimate = integrate.quad(lambda t: get_product_M(k, theta, t, previous_utility), 0, np.inf)
-        return integral_value 
+        l1_R2DP_upto_t=0
+        if len(previous_utility)>0:
+            l1_R2DP_upto_t= np.sum([values['l1_R2DP'] for key, values in previous_utility.items()])
 
-    def get_epsilon_R2DP(self, t, k, theta, delta, previous_epsilons):
+            # In case no positive epsilon found, and we have previous epsilons, we sum up and select the last alpha 
+
+
+        
+        return integral_value + l1_R2DP_upto_t
+
+    def get_epsilon_R2DP(self, t, k, theta, alpha, delta, previous_epsilons):
         """
         Computed as min_{alpha in 2:200} frac{1}{alpha-1} log[  frac{alpha}{2 alpha-1} prod_{t=1}^T (1-(alpha-1)theta_t)^{-k_t} + frac{log (1/delta)}{alpha-1}]
         
         return epsilon value for R2DP Mechanism
         """
 
-        def get_epsilon(alpha, k, theta, delta):
+        def get_epsilon(k, theta, alpha, delta):
             """
             This is inline funciton.
             it calculates frac{1}{alpha-1} log[  frac{alpha}{2 alpha-1} prod_{t=1}^T (1-(alpha-1)theta_t)^{-k_t} + frac{log (1/delta)}{alpha-1}
@@ -98,32 +106,32 @@ class DynamicMomentR2DP:
             values =np.multiply((1/(alpha-1)) , log_output)
             return values
         
-        alphas_less_than_1_over_delta= [alpha for alpha in self.DEFAULT_ALPHAS if alpha < 1/theta]
+        # alphas_less_than_1_over_delta= [alpha for alpha in self.DEFAULT_ALPHAS if alpha < 1/theta]
 
         # if len(alphas_less_than_1_over_delta) ==0:
         #     alphas_less_than_1_over_delta=[random.uniform(0.0001, 1/theta) for _ in range(10)]
 
 
-        all_epsilon_values=[ get_epsilon(alpha, k, theta, delta) for alpha in alphas_less_than_1_over_delta]
-        
-        positive_epsilons = [(value, idx) for idx, value in enumerate(all_epsilon_values) if value > 0]
+        # all_epsilon_values=[ get_epsilon(alpha, k, theta, delta) for alpha in alphas_less_than_1_over_delta]
+        epsilon=get_epsilon(k, theta, alpha, delta)
+        # positive_epsilons = [(value, idx) for idx, value in enumerate(all_epsilon_values) if value > 0]
 
-        if not positive_epsilons:
-            if len(previous_epsilons)>0:
-                epsilon_upto_t= np.sum([values['epsilon_R2DP'] for key, values in previous_epsilons.items()])
-                all_alphas=[values['alpha'] for key, values in previous_epsilons.items()]
-                last_alpha=all_alphas[-1]
-                # In case no positive epsilon found, and we have previous epsilons, we sum up and select the last alpha 
-                return epsilon_upto_t,   last_alpha 
-            return None, None 
+        # if not positive_epsilons:
+        #     if len(previous_epsilons)>0:
+        #         epsilon_upto_t= np.sum([values['epsilon_R2DP'] for key, values in previous_epsilons.items()])
+        #         all_alphas=[values['alpha'] for key, values in previous_epsilons.items()]
+        #         last_alpha=all_alphas[-1]
+        #         # In case no positive epsilon found, and we have previous epsilons, we sum up and select the last alpha 
+        #         return epsilon_upto_t,   last_alpha 
+        #     return None, None 
         
-        min_epsilon, min_alpha = min(positive_epsilons, key=lambda x: x[0])
+        # min_epsilon, min_alpha = min(positive_epsilons, key=lambda x: x[0])
 
-        epsilon_upto_t=0
-        if len(previous_epsilons)>0:
-            epsilon_upto_t= np.sum([values['epsilon_R2DP'] for key, values in previous_epsilons.items()])
-        total_epsilon=epsilon_upto_t + min_epsilon 
-        return total_epsilon, min_alpha
+        # epsilon_upto_t=0
+        # if len(previous_epsilons)>0:
+        #     epsilon_upto_t= np.sum([values['epsilon_R2DP'] for key, values in previous_epsilons.items()])
+        # total_epsilon=epsilon_upto_t  
+        return epsilon 
 
     def get_usefullness_Gaussian(self, epsilon, delta, sigma, sensitivity=1 ):
 
@@ -144,6 +152,7 @@ class DynamicMomentR2DP:
         
         def E():
             # gamma=(sensitivity/epsilon) * np.log(1/delta)
+            
             gamma = 0.1
             values = [ random.uniform(0,1) * Q(gamma/sigma) for _ in range(50)]
 
@@ -173,6 +182,8 @@ class DynamicMomentR2DP:
         return all paramters optimal value 
 
         """
+
+        valid_paramters=[(k, theta, alpha) for alpha in self.DEFAULT_ALPHAS for theta in self.THETA for k in self.K if alpha < (1/theta) and k > ((-1) * (np.log(2*alpha-1)/alpha) / (np.log(1-(alpha-1) * theta)))]
         t=1
         epsilon_R2DP=0
         l1_R2DP=0
@@ -183,54 +194,131 @@ class DynamicMomentR2DP:
             sigma=self.get_optimum_sigma_gaussian(t, epsilon_Gaussian_t, delta)
             l1_R2DP_optimal=float("inf")
             best_params={}
-            for k in self.K:
 
-                for theta in self.THETA:
 
-                    epsilon_R2DP_t, best_alpha = self.get_epsilon_R2DP(t, k, theta, delta, previous_epsilons_utility )
-                    
-                    if epsilon_R2DP_t==None:
-                        continue
+            for k_theta_alpha in valid_paramters:
+                k=k_theta_alpha[0]
+                theta=k_theta_alpha[1]
+                alpha=k_theta_alpha[2]
 
-                    if epsilon_R2DP_t < epsilon_Gaussian_t:
+                epsilon_R2DP_t = self.get_epsilon_R2DP(t, k, theta, alpha, delta, previous_epsilons_utility )
+                
+                if epsilon_R2DP_t==None or epsilon_R2DP_t<0:
+                    continue
 
-                        l1_R2DP=self.get_l1_R2DP( k, theta, previous_epsilons_utility)
+                if epsilon_R2DP_t < epsilon_Gaussian_t:
 
-                        l1_Gaussian=self.get_l1_Gaussian(sigma, t) # optimum sigma value based on epsilon
+                    l1_R2DP=self.get_l1_R2DP( k, theta, previous_epsilons_utility)
 
-                        usefulness_R2DP=self.get_usefullness_R2DP(k, theta, epsilon_R2DP_t, delta)
-                        usefulness_Gaussian=self.get_usefullness_Gaussian(epsilon_Gaussian_t, delta, sigma)
+                    l1_Gaussian=self.get_l1_Gaussian(sigma, t) # optimum sigma value based on epsilon
 
-                        if l1_R2DP < l1_R2DP_optimal:
-                            l1_R2DP_optimal=l1_R2DP
-                            epsilon_R2DP=epsilon_R2DP_t
-                            best_params={
-                                'k':k, 
-                                'theta':theta, 
-                                'alpha': best_alpha,
-                                'l1_R2DP': l1_R2DP_optimal,
-                                'epsilon_R2DP': epsilon_R2DP_t, 
-                                'useful_R2DP': usefulness_R2DP, 
-                                'l1_Gaussian': l1_Gaussian,
-                                'epsilon_Gaussian': epsilon_Gaussian_t ,
-                                'useful_Gaussian':usefulness_Gaussian
-                                }
-                            # previous_epsilons_utility.update({t:{
-                            #     'k':k, 
-                            #     'theta':theta, 
-                            #     'alpha': best_alpha,
-                            #     'l1_R2DP': l1_R2DP_optimal,
-                            #     'epsilon_R2DP': epsilon_R2DP_t, 
-                            #     'useful_R2DP': usefulness_R2DP, 
-                            #     'l1_Gaussian': l1_Gaussian,
-                            #     'epsilon_Gaussian': epsilon_Gaussian_t ,
-                            #     'useful_Gaussian':usefulness_Gaussian
-                            # }})
+                    usefulness_R2DP=self.get_usefullness_R2DP(k, theta, epsilon_R2DP_t, delta)
+                    usefulness_Gaussian=self.get_usefullness_Gaussian(epsilon_Gaussian_t, delta, sigma)
+
+                    if l1_R2DP < l1_R2DP_optimal:
+                        l1_R2DP_optimal=l1_R2DP
+                        epsilon_R2DP=epsilon_R2DP_t
+                        best_params={
+                            'k':k, 
+                            'theta':theta, 
+                            'alpha': alpha,
+                            'l1_R2DP': l1_R2DP_optimal,
+                            'epsilon_R2DP': epsilon_R2DP_t, 
+                            'useful_R2DP': usefulness_R2DP, 
+                            'l1_Gaussian': l1_Gaussian,
+                            'epsilon_Gaussian': epsilon_Gaussian_t ,
+                            'useful_Gaussian':usefulness_Gaussian
+                            }
+                        # previous_epsilons_utility.update({t:{
+                        #     'k':k, 
+                        #     'theta':theta, 
+                        #     'alpha': best_alpha,
+                        #     'l1_R2DP': l1_R2DP_optimal,
+                        #     'epsilon_R2DP': epsilon_R2DP_t, 
+                        #     'useful_R2DP': usefulness_R2DP, 
+                        #     'l1_Gaussian': l1_Gaussian,
+                        #     'epsilon_Gaussian': epsilon_Gaussian_t ,
+                        #     'useful_Gaussian':usefulness_Gaussian
+                        # }})
             if len(best_params)>0:
                 previous_epsilons_utility.update({t:best_params})                       
             t=t+1
 
         return previous_epsilons_utility
+
+    # def get_R2DP_nosies(self, sigma, delta, total_epsilon):
+    #     """
+    #     sigma: 
+    #     delta: 
+    #     total_epsilon: budget 
+        
+
+    #     It captures optimal k, theta, alpha, l1_R2DP, l1_Gaussian and store in a variable 
+
+        
+    #     return all paramters optimal value 
+
+    #     """
+
+    #     valid_paramters=[(k, theta, alpha) for alpha in self.DEFAULT_ALPHAS for theta in self.THETA for k in self.K if alpha < (1/theta) and k > ((-1) * (np.log(2*alpha-1)/alpha) / (np.log(1-(alpha-1) * theta)))]
+    #     t=1
+    #     epsilon_R2DP=0
+    #     l1_R2DP=0
+    #     previous_epsilons_utility={}
+    #     while epsilon_R2DP <= total_epsilon:
+
+    #         epsilon_Gaussian_t= self.get_epsilon_gaussian(t, sigma, delta)
+    #         sigma=self.get_optimum_sigma_gaussian(t, epsilon_Gaussian_t, delta)
+    #         l1_R2DP_optimal=float("inf")
+    #         best_params={}
+    #         for k in self.K:
+
+    #             for theta in self.THETA:
+
+    #                 epsilon_R2DP_t, best_alpha = self.get_epsilon_R2DP(t, k, theta, delta, previous_epsilons_utility )
+                    
+    #                 if epsilon_R2DP_t==None:
+    #                     continue
+
+    #                 if epsilon_R2DP_t < epsilon_Gaussian_t:
+
+    #                     l1_R2DP=self.get_l1_R2DP( k, theta, previous_epsilons_utility)
+
+    #                     l1_Gaussian=self.get_l1_Gaussian(sigma, t) # optimum sigma value based on epsilon
+
+    #                     usefulness_R2DP=self.get_usefullness_R2DP(k, theta, epsilon_R2DP_t, delta)
+    #                     usefulness_Gaussian=self.get_usefullness_Gaussian(epsilon_Gaussian_t, delta, sigma)
+
+    #                     if l1_R2DP < l1_R2DP_optimal:
+    #                         l1_R2DP_optimal=l1_R2DP
+    #                         epsilon_R2DP=epsilon_R2DP_t
+    #                         best_params={
+    #                             'k':k, 
+    #                             'theta':theta, 
+    #                             'alpha': best_alpha,
+    #                             'l1_R2DP': l1_R2DP_optimal,
+    #                             'epsilon_R2DP': epsilon_R2DP_t, 
+    #                             'useful_R2DP': usefulness_R2DP, 
+    #                             'l1_Gaussian': l1_Gaussian,
+    #                             'epsilon_Gaussian': epsilon_Gaussian_t ,
+    #                             'useful_Gaussian':usefulness_Gaussian
+    #                             }
+    #                         # previous_epsilons_utility.update({t:{
+    #                         #     'k':k, 
+    #                         #     'theta':theta, 
+    #                         #     'alpha': best_alpha,
+    #                         #     'l1_R2DP': l1_R2DP_optimal,
+    #                         #     'epsilon_R2DP': epsilon_R2DP_t, 
+    #                         #     'useful_R2DP': usefulness_R2DP, 
+    #                         #     'l1_Gaussian': l1_Gaussian,
+    #                         #     'epsilon_Gaussian': epsilon_Gaussian_t ,
+    #                         #     'useful_Gaussian':usefulness_Gaussian
+    #                         # }})
+    #         if len(best_params)>0:
+    #             previous_epsilons_utility.update({t:best_params})                       
+    #         t=t+1
+
+    #     return previous_epsilons_utility
 
 
 
